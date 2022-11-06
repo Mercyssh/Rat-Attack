1dde377345bcea22b242acc14685f6a4f7e4df85
Mercyssh <ima.omrane@gmail.com>
2022-11-07 03:23:43 +0530

    update 1

---
+++ index.js
@@ -0,0 +1,6 @@
+function loadRats() {
+    for (var rat of data) {
+
+    }
+}
+loadRats(); //Initialize
---
+++ reports.js
@@ -0,0 +1,16 @@
+/* Template :
+{
+    id: int, 
+    description: string, 
+    by: string
+}
+*/
+data = [
+    { id: 0, wing: "a", description: "oh no", by: "testMan" },
+    { id: 1, wing: "b", description: "i saw it!", by: "testMan2" },
+    { id: 2, wing: "c", description: "it ATTACKED ME", by: "testMan3" },
+    { id: 3, wing: "d", description: "UM", by: "tessdan4" },
+    { id: 4, wing: "a", description: "OOPS", by: "kapouta" },
+    { id: 5, wing: "a", description: "aaah", by: "manohman" },
+    { id: 6, wing: "d", description: "bruh moment", by: "SHAME" },
+]
