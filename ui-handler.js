686e775aa4f98b33d2076b6d2cefd9f21c173851
Mercyssh <ima.omrane@gmail.com>
2022-11-07 16:29:44 +0530

    Updated Wing / Floor Radio UI elements
    visuals only, logic yet to come!

--- ui-handler.js
+++ ui-handler.js
@@ -3,6 +3,8 @@
 var reportBtn = document.querySelector("#reportBtn");
 var cancelBtn = document.querySelector("#cancelBtn");
 var submitBtn = document.querySelector("submitBtn");
+var wingInputs = document.querySelectorAll(".wingBtn");
+var floorInputs = document.querySelectorAll(".floorBtn");
 
 // Input Fields
 var nameInput = document.querySelector(".nameInput input");
@@ -11,6 +13,8 @@ var descInput = document.querySelector(".descInput textarea");
 // others
 var reportForm = document.querySelector("#reportForm");
 
+
+//###OPEN AND CLOSE FORM###
 // Open & Close Buttons : Attach Event Handlers
 function openForm() {
     reportForm.classList.add("show");
@@ -20,4 +24,12 @@ function closeForm() {
 }
 reportBtn.addEventListener("click", openForm);
 cancelBtn.addEventListener("click", closeForm);
+//#########################
 
+
+//###########WING INPUT####
+// Updates the wingInput when called
+function updateWingInput(ele) {
+
+}
+//#########################
