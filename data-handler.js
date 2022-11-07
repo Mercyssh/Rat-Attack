4a3e2b78b9c9b7b4149c94580ca6bfb8f11a2d24
Mercyssh <ima.omrane@gmail.com>
2022-11-08 04:41:21 +0530

    Supabase initialized!
    we can now pull reports and push in new ones from the report form

--- data-handler.js
+++ data-handler.js
@@ -1,8 +1,60 @@
+//Initialize SupaBase
+const { createClient } = supabase;
+const supabaseUrl = 'https://srhvetdcccijazdjhwnk.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNyaHZldGRjY2NpamF6ZGpod25rIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njc4MTQ2NzgsImV4cCI6MTk4MzM5MDY3OH0.I2m0Nbhpfw6YyoV3UB1nk6ZlrSTR5bTnJf9kgFFtZ0M';
+const _supabase = createClient(supabaseUrl, supabaseKey);
+
+localData = null;
+
 // Call function to push everything inside the form
-function pushReport() {
+async function pushReport() {
+    //Only push if allowed to submit!
+    var _d = new Date;
 
+    //Submit data if allowed
+    if (allowSubmit) {
+        const { data, error } = await _supabase
+            .from('reports')
+            .insert([{
+                id: Date.now(),
+                created_at: _d,
+                wing: wingValue,
+                floor: floorValue,
+                description: descInput.value,
+                name: nameInput.value
+            }]).select()
+
+        // Fail :(
+        if (error) {
+            console.log(error);
+        }
+
+        // Success!
+        if (data) {
+            console.log("pushed successfully")
+            console.log(data);
+
+            pullReports();  //Update local copy again
+            closeForm();
+        }
+    }
 }
+submitBtn.addEventListener("click", pushReport);
 
-function pullReports() {
+//Pulls Data from the database
+async function pullReports() {
+    // Fetch Table
+    const { data, error } = await _supabase
+        .from('reports')
+        .select()
+
+    // Catch error
+    if (error) { alert("Can't retrieve reports! Contact Om") }
+    if (data) { console.log(data); localData = data };
+}
+pullReports();
+
+// loads reports into UI
+function loadReports() {
 
 }
