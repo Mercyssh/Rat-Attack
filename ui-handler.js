35385e76c748ab78ad767d7eb5d5267c5559cf15
Mercyssh <ima.omrane@gmail.com>
2022-11-07 16:20:54 +0530

    backing up b4 i break stuff
    uh oh

--- ui-handler.js
+++ ui-handler.js
@@ -1,12 +1,17 @@
 // Grab handles
+// Buttons
 var reportBtn = document.querySelector("#reportBtn");
 var cancelBtn = document.querySelector("#cancelBtn");
 var submitBtn = document.querySelector("submitBtn");
 
+// Input Fields
+var nameInput = document.querySelector(".nameInput input");
+var descInput = document.querySelector(".descInput textarea");
+
+// others
 var reportForm = document.querySelector("#reportForm");
 
-
-
+// Open & Close Buttons : Attach Event Handlers
 function openForm() {
     reportForm.classList.add("show");
 }
@@ -15,3 +20,4 @@ function closeForm() {
 }
 reportBtn.addEventListener("click", openForm);
 cancelBtn.addEventListener("click", closeForm);
+
